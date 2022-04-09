2ecf65fc955f5b5c5ab6c6ab7e7336a2dfb554e1
Loiren <84875764+Loirenn@users.noreply.github.com>
2022-04-09 11:04:01 +0300

    Update Footer.tsx

--- components/Static/Footer.tsx
+++ components/Static/Footer.tsx
@@ -2,7 +2,9 @@ export default function Footer() {
     return (
         <div className="w-full flex items-center justify-between">
             <h1 className="font-light text-white">Designed with <i className="fal fa-heart mx-1 text-blue-400" /> by Templatet</h1>
-            <h1 className="font-light text-white hover:text-blue-400 transition-all cursor-pointer">This is an open source project.</h1>
+            <a target="_blank" href="https://github.com/templatet/nextjs-with-tailwind">
+                <h1 className="font-light text-white hover:text-blue-400 transition-all cursor-pointer">This is an open source project.</h1>
+            </a>
         </div>
     );
 };
